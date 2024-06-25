2e0d9d39eba19ce7f23f07091035dc9872cb990c
bbonugba <162338701+bbonugba@users.noreply.github.com>
2024-06-25 11:38:55 +0100

    Home Page done ☑️

--- index.php
+++ index.php
@@ -13,14 +13,12 @@
             <div class="container">
                 <h2 class="grey-text text-darken-2">Welcome to Scoups!</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, reprehenderit corporis! Recusandae, magni sed vel magnam provident iusto quis laboriosam, esse, ratione fugiat consequuntur unde voluptate dolore temporibus voluptas molestias. Placeat vero officiis, perspiciatis mollitia, quae beatae, sed odit recusandae quasi vitae id. Ducimus, magnam consequatur nostrum quae reiciendis quas?</p>
-                <ul class="collection">
-                    <li class="collection-item avatar">
-                        <img src="Images/onugbu.jpeg" alt="" class="responsive-img circle">
-                        <h6 class="grey-text text-darken-2"><strong>Onugbu Soup</strong></h6>
-                    </li>
-                    <li class="collection-item avatar"></li>
-                    <li class="collection-item avatar"></li>
-                </ul>
+                <div class="collection">
+                    <a href="" class="collection-item lime-text text-darken-4">Onugbu</a>
+                    <a href="" class="collection-item lime-text text-darken-4">Orah</a>
+                    <a href="" class="collection-item lime-text text-darken-4">Afang</a>
+                    <a href="" class="collection-item lime-text text-darken-4">Edikang-Ikong</a>
+                </div>
             </div>
         </section>
     </main>
